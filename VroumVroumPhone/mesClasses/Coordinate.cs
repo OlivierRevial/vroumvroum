387f955840eac4f1d3030f2a20f8d1f66d06513f
noex1987 <samuel.verron@ingesup.com>
2014-01-24 23:41:47 +0100

    Bibliothèque de classes - Schema adresse

---
+++ VroumVroumPhone/mesClasses/Coordinate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mesClasses
+{
+    class Coordinate
+    {
+        public int id { get; set; }
+        public decimal longitude { get; set; }
+        public decimal latitude { get; set; }
+        public Coordinate(decimal _longitude, decimal _latitude)
+        {
+            this.longitude = _longitude;
+            this.latitude = _latitude;
+        }
+        public Coordinate(int _id, decimal _longitude, decimal _latitude)
+        {
+            this.id = _id;
+            this.longitude = _longitude;
+            this.latitude = _latitude;
+        }
+    }
+}
---
+++ VroumVroumPhone/mesClasses/Departement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mesClasses
+{
+    class Departement
+    {
+        public string name{get;set;}
+        public int id { get; set; }
+        public int regionId { get; set; }
+        public Departement(string _name, int _regionId)
+        {
+            this.name = _name;
+            this.regionId = _regionId;
+        }
+        public Departement(int _id, string _name, int _regionId)
+        {
+            this.id = _id;
+            this.name = _name;
+            this.regionId = _regionId;
+        }
+    }
+}
