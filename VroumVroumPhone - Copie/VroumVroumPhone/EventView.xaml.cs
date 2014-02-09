36487b291328d84761d03bf7c53720d30e5ba41a
noex1987 <samuel.verron@ingesup.com>
2014-02-09 20:09:07 +0100

    nettoyage

---
+++ VroumVroumPhone - Copie/VroumVroumPhone/EventView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace VroumVroumPhone
+{
+    public partial class EventView : PhoneApplicationPage
+    {
+        public EventView()
+        {
+            InitializeComponent();
+        }
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            string eventId = this.NavigationContext.QueryString["parameter"];
+            id.Text = eventId;
+            //this.DataContext = new EventViewModel();
+
+        }
+    }
+}
---
+++ VroumVroumPhone - Copie/VroumVroumPhone/EventsView.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.Collections.ObjectModel;
+
+namespace VroumVroumPhone
+{
+    public partial class EventsView : PhoneApplicationPage
+    {
+        public EventsView()
+        {
+            InitializeComponent();
+            //this.DataContext = new EventsViewModel();
+        }
+
+        private void StackPanel_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            int id = (int)(sender as StackPanel).Tag;
+            try
+            {
+                NavigationService.Navigate(new Uri(String.Format("/EventView.xaml?parameter={0}", id), UriKind.Relative));
+            }
+            catch (Exception)
+            { }    
+        }
+        
+    }
+}
